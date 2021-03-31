c2c2198ea1437401b5385b213a34857f78e25526
adjoined-tb <adjoined.tb@gmail.com>
2021-03-30 22:50:48 -0400

    Service Discovery

--- gulimall-ware/src/main/java/me/adjoined/gulimall/ware/GulimallWareApplication.java
+++ gulimall-ware/src/main/java/me/adjoined/gulimall/ware/GulimallWareApplication.java
@@ -1,4 +1,4 @@
-package me.adjoined.gulimall.coupon;
+package me.adjoined.gulimall.ware;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class GulimallCouponApplication {
+public class GulimallWareApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GulimallCouponApplication.class, args);
+        SpringApplication.run(GulimallWareApplication.class, args);
     }
 
 }
