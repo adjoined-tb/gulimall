3c4ab62fe3310eec5ce5778e80f4cee2de83e14b
adjoined-tb <adjoined.tb@gmail.com>
2021-04-02 16:25:20 -0400

    before es work

---
+++ gulimall-search/src/test/java/me/adjoined/gulimall/search/GulimallSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package me.adjoined.gulimall.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
