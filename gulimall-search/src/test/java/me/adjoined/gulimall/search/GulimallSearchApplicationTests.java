912ae7de0bb4958a376ec3443af3e142120f147a
adjoined-tb <adjoined.tb@gmail.com>
2021-04-02 17:29:45 -0400

    es query

--- gulimall-search/src/test/java/me/adjoined/gulimall/search/GulimallSearchApplicationTests.java
+++ gulimall-search/src/test/java/me/adjoined/gulimall/search/GulimallSearchApplicationTests.java
@@ -1,13 +1,83 @@
 package me.adjoined.gulimall.search;
 
+import com.google.gson.Gson;
+import lombok.Data;
+import lombok.ToString;
+import me.adjoined.gulimall.search.config.GulimallElasticSearchConfig;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.aggregations.metrics.AvgAggregationBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.comparator.JSONCompareUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.util.Map;
 
 @SpringBootTest
 class GulimallSearchApplicationTests {
 
+    @Autowired
+    private RestHighLevelClient esClient;
     @Test
     void contextLoads() {
+        System.out.println(esClient);
     }
+
+    @Test
+    void indexData() throws IOException {
+        IndexRequest indexRequest = new IndexRequest("users");
+        indexRequest.id("1");
+//        indexRequest.source("userName", "zhanggsan", "age", "18", "gender", "M");
+        User user = new User();
+        user.setUserName("zhangsan");
+        user.setAge(18);
+        user.setGender("M");
+        String jsonStr = new Gson().toJson(user);
+        indexRequest.source(jsonStr, XContentType.JSON);
+
+        IndexResponse res = esClient.index(indexRequest, GulimallElasticSearchConfig.COMMON_OPTIONS);
+        System.out.println(res);
+    }
+
+    @Data
+    class User{
+        private String userName;
+        private String gender;
+        private Integer age;
+    }
+
+    @Test
+    void searchData() throws IOException {
+        SearchRequest searchRequest =  new SearchRequest("bank");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchQuery("address", "mill"));
+        TermsAggregationBuilder ageAgg = AggregationBuilders.terms("ageAgg").field("age").size(10);
+        searchSourceBuilder.aggregation(ageAgg);
+
+        AvgAggregationBuilder balanceAvg = AggregationBuilders.avg("balanceAvg").field("balance");
+        searchSourceBuilder.aggregation(balanceAvg);
+
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse res = esClient.search(searchRequest, GulimallElasticSearchConfig.COMMON_OPTIONS);
+        System.out.println(res);
+//        new Gson().fromJson(res.toString(), Map.class);
+        for (SearchHit hit : res.getHits().getHits()) {
+            hit.getSourceAsString()
+        }
+    }
+
+
 
 }
