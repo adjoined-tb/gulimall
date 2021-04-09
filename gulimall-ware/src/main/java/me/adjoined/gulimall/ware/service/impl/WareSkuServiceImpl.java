c0fcee6750c4a832dbf7402a4f06de32829b659e
adjoined-tb <adjoined.tb@gmail.com>
2021-04-08 22:51:43 -0400

    warehouse auto release locked stock

--- gulimall-ware/src/main/java/me/adjoined/gulimall/ware/service/impl/WareSkuServiceImpl.java
+++ gulimall-ware/src/main/java/me/adjoined/gulimall/ware/service/impl/WareSkuServiceImpl.java
@@ -1,7 +1,17 @@
 package me.adjoined.gulimall.ware.service.impl;
 
 import me.adjoined.common.exception.NoStockException;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.rabbitmq.client.Channel;
+
+import java.io.IOException;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -14,9 +24,12 @@ import me.adjoined.gulimall.ware.entity.WareSkuEntity;
 import me.adjoined.gulimall.ware.service.WareSkuService;
 import org.springframework.transaction.annotation.Transactional;
 
-
+@RabbitListener(queues = {"stock.release.stock.queue"})
 @Service("wareSkuService")
 public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> implements WareSkuService {
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -45,7 +58,26 @@ public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> i
         if (!this.save(entity2)) {
             throw new NoStockException(222L);
         }
+
+        rabbitTemplate.convertAndSend("stock-event-exchange",
+                "stock.locked", "lock the stock for 2 minutes");
 //        throw new NoStockException(333L);
+    }
+
+    @RabbitHandler
+    public void handleStockLockedRelease(String content, Message message, Channel channel) {
+        byte[] body = message.getBody();
+        MessageProperties properties = message.getMessageProperties();
+        System.out.println("got " + message + " type: " + message.getClass());
+        System.out.println("getDeliveryTag " + properties.getDeliveryTag());
+
+        System.out.println("content: " + content);
+        try {
+            channel.basicAck(properties.getDeliveryTag(), false);
+        } catch (IOException e) {
+            // Network issue
+            e.printStackTrace();
+        }
     }
 
 }
